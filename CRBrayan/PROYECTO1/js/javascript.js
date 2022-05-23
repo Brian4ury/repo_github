45bd71d92304604e43bc1c3ed035ef2638792a5b
Brian4ury <maykel010203@gmail.com>
2022-05-23 10:40:13 +0200

    Hemos incluido la carpeta CBRBrayan que tiene todos los proyectos

---
+++ CRBrayan/PROYECTO1/js/javascript.js
@@ -0,0 +1,22 @@
+//var Name=const MiBoton = () =>{
+  //    let inputValue = document.getElementById("username").value; 
+
+
+
+
+function MiBoton(){
+    
+    var Name=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    
+    if(Name=="Brianfury" && password=="12345"){
+   alert("Es correcto");
+        
+   }
+    
+else{
+    alert("Los datos son incorrectos");
+}
+
+}
+
